5fca43469e07b134b7e749c215b9ee03e04de84a
Adoo <Adoo@outlook.com>
2024-02-03 02:27:17 +0800

    alpha 3

--- rclog_hook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Merging changelog: $MERGE_CHANGELOG"
-if [ "$MERGE_CHANGELOG" = true ]; then
-  echo "Starging merge changelog"
-  echo "$(rclog -t $NEW_VERSION -p ./CHANGELOG.md merge)" >| ./CHANGELOG.md
-fi
